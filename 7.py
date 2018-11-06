74de83d33542328e21f4ea4a359994a000228b22
Kaylamck202 <kaylamck202@gmail.com>
2018-11-06 13:41:00 -0600

    consec_lett

---
+++ 7.py
@@ -0,0 +1,10 @@
+def consec_lett():
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.strip().split():
+                for i in range(len(word)-5):
+                    if word[i] == word[i+1]and word[i+2]==word[i+3]and word[i+4]==word[i+5]:
+                        print(word)
+                        
+if __name__ == "__main__":
+    consec_lett()
